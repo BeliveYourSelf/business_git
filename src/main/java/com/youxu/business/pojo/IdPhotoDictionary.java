84af339313ee2752774228980161735acb33e786
0101 <454886565@qq.com>
2019-09-06 14:27:16 +0800

    1.第3次提交 证件照制作

--- src/main/java/com/youxu/business/pojo/IdPhotoDictionary.java
+++ src/main/java/com/youxu/business/pojo/IdPhotoDictionary.java
@@ -1,7 +1,9 @@
 package com.youxu.business.pojo;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class IdPhotoDictionary {
     private Integer id;
 
@@ -9,57 +11,13 @@ public class IdPhotoDictionary {
 
     private Integer idPhotoDictionarySpecId;
 
+    private Double idPhotoDictionaryPrice;
+
     private Date idPhotoDictionaryCreateTime;
 
     private Date idPhotoDictionaryModifyTime;
 
     private Boolean idPhotoDictionaryStatus;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getIdPhotoDictionaryName() {
-        return idPhotoDictionaryName;
-    }
-
-    public void setIdPhotoDictionaryName(String idPhotoDictionaryName) {
-        this.idPhotoDictionaryName = idPhotoDictionaryName == null ? null : idPhotoDictionaryName.trim();
-    }
-
-    public Integer getIdPhotoDictionarySpecId() {
-        return idPhotoDictionarySpecId;
-    }
-
-    public void setIdPhotoDictionarySpecId(Integer idPhotoDictionarySpecId) {
-        this.idPhotoDictionarySpecId = idPhotoDictionarySpecId;
-    }
-
-    public Date getIdPhotoDictionaryCreateTime() {
-        return idPhotoDictionaryCreateTime;
-    }
-
-    public void setIdPhotoDictionaryCreateTime(Date idPhotoDictionaryCreateTime) {
-        this.idPhotoDictionaryCreateTime = idPhotoDictionaryCreateTime;
-    }
-
-    public Date getIdPhotoDictionaryModifyTime() {
-        return idPhotoDictionaryModifyTime;
-    }
-
-    public void setIdPhotoDictionaryModifyTime(Date idPhotoDictionaryModifyTime) {
-        this.idPhotoDictionaryModifyTime = idPhotoDictionaryModifyTime;
-    }
-
-    public Boolean getIdPhotoDictionaryStatus() {
-        return idPhotoDictionaryStatus;
-    }
-
-    public void setIdPhotoDictionaryStatus(Boolean idPhotoDictionaryStatus) {
-        this.idPhotoDictionaryStatus = idPhotoDictionaryStatus;
-    }
 }

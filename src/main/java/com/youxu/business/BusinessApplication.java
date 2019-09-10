3c40a897b1943bbc071d7365e44d87e9b6eb2713
eddy <xqy@lishengtj.com>
2019-09-10 17:29:25 +0800

    修改生成的代码

--- src/main/java/com/youxu/business/BusinessApplication.java
+++ src/main/java/com/youxu/business/BusinessApplication.java
@@ -4,14 +4,16 @@ import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Properties;
 
-@SpringBootApplication
+@EnableSwagger2
 @MapperScan("com.youxu.business.dao")
-@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
 public class BusinessApplication {
 
 	public static void main(String[] args) {

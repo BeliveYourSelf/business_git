6285c565bb57dc5c7170cc1e6bcfcaa4346bafd2
0101 <454886565@qq.com>
2019-09-17 14:21:16 +0800

    1.swagger

--- src/main/java/com/youxu/business/service/ShareService.java
+++ src/main/java/com/youxu/business/service/ShareService.java
@@ -2,6 +2,8 @@ package com.youxu.business.service;
 
 import com.youxu.business.pojo.Share;
 
+import java.text.ParseException;
+
 public interface ShareService {
-    Integer insertShare(Share share);
+    Integer insertShare(Share share) throws ParseException;
 }
--- src/main/java/com/youxu/business/service/impl/ShareServiceImpl.java
+++ src/main/java/com/youxu/business/service/impl/ShareServiceImpl.java
@@ -6,6 +6,7 @@ import com.youxu.business.pojo.Share;
 import com.youxu.business.service.AccessTokenService;
 import com.youxu.business.service.ShareService;
 import com.youxu.business.utils.uuid.TimeProblem;
+import com.youxu.business.utils.uuid.UUIDUtils;
 import com.youxu.business.utils.wechat.qrcode.MiniAppCode;
 import com.youxu.business.utils.wechat.requestapitool.RequestApiTool;
 import org.slf4j.Logger;
@@ -26,18 +27,27 @@ public class ShareServiceImpl implements ShareService {
     @Resource
     private AccessTokenService accessTokenService;
     @Override
-    public Integer insertShare(Share share) {
+    public Integer insertShare(Share share) throws ParseException {
         //更新accessToken
         String shareUserId = share.getShareUserId().toString();
-        StringBuffer userId = new StringBuffer(shareUserId);
 
-      /*  userId+","+
-        ShareServiceImpl shareService = new ShareServiceImpl();*/
-//        shareService.updateAccessTokenAndCreateQRcode();
+        StringBuffer userId = new StringBuffer(shareUserId);
+        String shareCode = UUIDUtils.generateShortUuid();
+        String userIdAndInvitationCode = userId + "," + shareCode;
+
+        ShareServiceImpl shareService = new ShareServiceImpl();
+        String qrCodePath = shareService.updateAccessTokenAndCreateQRcode(userIdAndInvitationCode);
+        share.setQrCode(qrCodePath);
         return shareMapper.insertShare(share);
     }
 
-    public void updateAccessTokenAndCreateQRcode(String userIdAndInvitationCode) throws ParseException {
+    /**
+     * 生成二维码
+     * @param userIdAndInvitationCode
+     * @return
+     * @throws ParseException
+     */
+    public String updateAccessTokenAndCreateQRcode(String userIdAndInvitationCode) throws ParseException {
         AccessToken accessToken = accessTokenService.selectAccessToken();
         String modifyTimeString = accessToken.getModifyTimeString();
         SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -62,7 +72,7 @@ public class ShareServiceImpl implements ShareService {
         MiniAppCode miniAppCode = new MiniAppCode();
         StringBuilder stringBuilderPath = miniAppCode.getminiqrQr(userIdAndInvitationCode, token);//上传图片路径
         String stringPath = stringBuilderPath.toString();
-
+    return stringPath;
     }
 
 
